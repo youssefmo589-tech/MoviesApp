e9c22168dc2112dca059192944906549ef150c62
youssefmo589-tech <youssefmo589@gmail.com>
2026-09-01 06:02:45 -0700

    ..

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(const Duration(seconds: 3), () {
       Navigator.pushNamedAndRemoveUntil(
-          context, AppRouteName.profile, (route) => false);
+          context, AppRouteName.onBoarding, (route) => false);
     });
   }
 

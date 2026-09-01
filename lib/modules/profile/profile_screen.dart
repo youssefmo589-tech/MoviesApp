b30352d60fa2cbc835d5bf33937bf04aa02032d8
Atallah <mohamedahmedatallah@gmail.com>
2026-09-01 16:49:17 +0300

    added new avatar bottom sheet

--- lib/modules/profile/profile_screen.dart
+++ lib/modules/profile/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import '../../core/app_routes/app_route_name.dart';
 import '../../core/app_theme_manager/app_colors.dart';
+import '../../widgets/avatar_bottom_sheet.dart';
 import '../../widgets/button_widget.dart';
 
 class Profile extends StatefulWidget {
@@ -11,8 +13,11 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+   int _selectedAvatar = -1;
   @override
   Widget build(BuildContext context) {
+
     final theme = Theme.of(context).textTheme;
 
     return Scaffold(
@@ -38,9 +43,177 @@ class _ProfileState extends State<Profile> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: 37),
-            CircleAvatar(
-              radius: 70,
-              backgroundImage: AssetImage("assets/images/gamer (1) (7).png"),
+            GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                    backgroundColor: Colors.transparent,
+                    isScrollControlled: true,
+                    isDismissible: true,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return StatefulBuilder(builder: (context, setState) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.grey,
+                              borderRadius: BorderRadius.circular(16),
+
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceAround,
+                                  children: [
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 1;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1).png"),
+                                        index: 1,
+                                        isSelected: _selectedAvatar == 1,),
+                                    )),
+                                    Expanded(
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            _selectedAvatar = 2;
+                                          });
+                                        },
+
+                                        child: AvatarBottomSheet(
+                                          image: Image.asset(
+                                              "assets/images/gamer (1) (1).png"),
+                                          index: 2,
+                                          isSelected: _selectedAvatar == 2,),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            _selectedAvatar = 3;
+                                          });
+                                        },
+
+                                        child: AvatarBottomSheet(
+                                          image: Image.asset(
+                                              "assets/images/gamer (1) (2).png"),
+                                          index: 3,
+                                          isSelected: _selectedAvatar == 3,),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceAround,
+
+                                  children: [
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 4;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (3).png"),
+                                        index: 4,
+                                        isSelected: _selectedAvatar == 4,),
+                                    )),
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 5;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (4).png"),
+                                        index: 5,
+                                        isSelected: _selectedAvatar == 5,),
+                                    )),
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 6;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (5).png"),
+                                        index: 6,
+                                        isSelected: _selectedAvatar == 6,),
+                                    )),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceAround,
+
+                                  children: [
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 7;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (6).png"),
+                                        index: 7,
+                                        isSelected: _selectedAvatar == 7,),
+                                    )),
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 8;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (7).png"),
+                                        index: 8,
+                                        isSelected: _selectedAvatar == 8,),
+                                    )),
+                                    Expanded(child: GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          _selectedAvatar = 9;
+                                        });
+                                      },
+                                      child: AvatarBottomSheet(
+                                        image: Image.asset(
+                                            "assets/images/gamer (1) (8).png"),
+                                        index: 9,
+                                        isSelected: _selectedAvatar == 9,),
+                                    )),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                      );
+
+                    }
+
+                );
+              },
+
+              child: CircleAvatar(
+                radius: 70,
+                backgroundImage: AssetImage("assets/images/gamer (1) (7).png"),
+              ),
             ),
 
             SizedBox(height: 35),
@@ -148,4 +321,6 @@ class _ProfileState extends State<Profile> {
       ),
     );
   }
+
+
 }

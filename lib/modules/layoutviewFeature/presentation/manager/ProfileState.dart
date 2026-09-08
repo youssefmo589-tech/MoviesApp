f8480e816b8468e5d923ca2c816a0c60dd7e6763
youssefmo589-tech <youssefmo589@gmail.com>
2026-09-08 15:49:34 -0700

    .

--- lib/modules/layoutviewFeature/presentation/manager/ProfileState.dart
+++ lib/modules/layoutviewFeature/presentation/manager/ProfileState.dart
@@ -1,30 +1,3 @@
-// part of 'ProfileBloc.dart';
-//
-// class ProfileState extends Equatable {
-//   const ProfileState();
-//
-//   @override
-//   // TODO: implement props
-//   List<Object?> get props => throw UnimplementedError();
-// }
-//
-// class InitialState extends ProfileState {
-//   const InitialState();
-// }
-//
-// class LoadingState extends ProfileState {
-//   const LoadingState();
-// }
-//
-// class ErrorState extends ProfileState {
-//   final String message;
-//
-//   const ErrorState({required this.message});
-// }
-//
-// class SuccessState extends ProfileState {
-//   const SuccessState();
-// }
 
 part of 'ProfileBloc.dart';
 

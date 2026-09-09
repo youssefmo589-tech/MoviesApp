19bb490b563707e97b0a843afecb270532d21797
walee <moaaw456@gmail.com>
2026-09-09 15:11:47 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:bot_toast/bot_toast.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:movieapp/modules/layoutviewFeature/presentation/pages/profile/presentation/home_screen.dart';
-import 'core/Services/EasyLoadingService.dart';
 import 'core/app_theme_manager/app_theme_manager.dart';
 import 'firebase_options.dart';
 
@@ -12,8 +9,6 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(MyApp());
-
-  configLoading();
 }
 
 class MyApp extends StatelessWidget {
@@ -22,11 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: EasyLoading.init(
-        builder: BotToastInit(),
-      ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+     home: const HomeScreen(),
       theme: AppThemeManager.theme,
     );
   }

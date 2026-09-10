f9f6dc05e0d8fd6034cd641ac79745c6227bbc67
walee <moaaw456@gmail.com>
2026-09-10 11:28:23 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:movieapp/modules/layoutviewFeature/presentation/pages/profile/presentation/home_screen.dart';
+import 'package:movieapp/core/app_routes/app_route_config.dart';
+import 'package:movieapp/core/app_routes/app_route_name.dart';
 import 'core/app_theme_manager/app_theme_manager.dart';
 import 'firebase_options.dart';
 
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: const HomeScreen(),
+      initialRoute: AppRouteName.initial,
+      onGenerateRoute: AppConfig.onGenerateRoute,
       theme: AppThemeManager.theme,
     );
   }

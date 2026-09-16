b99c1280d0654c48a6f12b17ab731e783778ec6a
walee <moaaw456@gmail.com>
2026-09-17 01:02:29 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:movieapp/core/app_routes/app_route_config.dart';
 import 'package:movieapp/core/app_routes/app_route_name.dart';
 import 'core/app_theme_manager/app_theme_manager.dart';
